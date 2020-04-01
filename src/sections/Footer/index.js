794df9e4e7dfde8a70e44105f31f1f27b58ba0ab
andrii <romeruk21@gmail.com>
2020-04-01 12:21:47 +0300

    added basic footer

---
+++ src/sections/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Container } from 'react-bootstrap';
+
+export const Footer = () => {
+  return (
+    <footer id="sticky-footer" class="py-4 bg-dark text-white-50">
+      <Container className="text-center">
+        <small>Copyright &copy; blog client</small>
+      </Container>
+
+    </footer>
+  )
+}
