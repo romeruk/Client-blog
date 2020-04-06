4235003f24a185c7e91c485716f5341407f40694
andrii <romeruk21@gmail.com>
2020-04-06 12:19:01 +0300

    add apollo-upload-client, client config changes

--- src/apollo/apolloClient.js
+++ src/apollo/apolloClient.js
@@ -1,17 +1,21 @@
-
-import ApolloClient from "apollo-boost";
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { onError } from 'apollo-link-error';
+import { ApolloLink } from 'apollo-link'
+import { createUploadLink } from 'apollo-upload-client';
 import { toast } from 'react-toastify';
 
 export const client = new ApolloClient({
-  credentials: 'include',
-  uri: '/graphql',
-  onError({ networkError }) {
-    if (networkError) {
-      toast("Network error", {
-        type: "error"
-      });
-
-      console.log(`[Network error]: ${networkError}`);
-    }
-  }
+  link: ApolloLink.from([
+    onError(({ networkError }) => {
+      if (networkError) {
+        toast("Network error", {
+          type: "error"
+        });
+      }
+    }),
+    createUploadLink({ uri: '/graphql', credentials: 'include' }),
+  ]),
+  cache: new InMemoryCache(),
+  defaultHttpLink: false
 });
